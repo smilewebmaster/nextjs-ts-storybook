586721fe91a6bd5ba5b9d3e9804b3b8053d50824
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2021-06-02 15:24:57 +0300

    Configure Storybook, TypeScript and SASS

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,11 @@
 module.exports = {
-  stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  addons: [
+  "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    {
-      name: "storypug",
-      options: {
-        babel: true, //use babel-loader
-        loaderOptions: {
-          root: "../stories", // use src components as the pug root inclusion path
-        },
-      },
-    },
-    "@whitespace/storybook-addon-html",
-  ],
-};
+    "@storybook/preset-create-react-app"
+  ]
+}
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Button.module.scss";
 
-interface ButtonProps {
+export interface ButtonProps {
   children: React.ReactNode;
 }
 
