44166a23f4a8d45900bb73c90e6877bb5c74f7d3
99smile99 <alex95jin@gmail.com>
2021-06-02 14:55:11 +0300

    Convert project to standard React App

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.scss";
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Hello</Button>
+    </div>
+  );
+}
+
+export default App;
